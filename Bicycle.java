6f8db22552c845b2fe2b252f70bc9f3dfce8926f
William Stobaugh <william.jamesstobaugh@gmail.com>
2020-11-05 18:08:03 -0800

    Main function and Bike class

---
+++ Bicycle.java
@@ -0,0 +1,14 @@
+public class Bicycle {
+
+    // class variables
+
+
+    // Bike init function, the syntax in order should be (color, size, wheel size, wheel width, seat hight, )
+    public static void Bicycle()
+    {
+
+
+    }
+    // add functions (may be removed and added to individual components)
+
+}
---
+++ BikeMain.java
@@ -0,0 +1,8 @@
+public class BikeMain {
+    public static void main(String[] args) {
+
+
+        // create bikea
+        Bicycle myBike = new Bicycle(); 
+    }
+  }
