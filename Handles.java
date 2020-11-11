699eb7a7ca856f5d6346f0905dfdb097c99f5d4a
sarah <23mins22@stu.smuhsd.org>
2020-11-10 17:38:36 -0800

    Add Handle.java

---
+++ Handles.java
@@ -0,0 +1,25 @@
+
+public class Handles {
+   
+    public static void Direction() {
+       System.out.println("the bike turns");
+       
+        /* boolean left = false;
+        boolean right = false;
+
+        if (left = true) {
+            System.out.println("bike turns left");
+        } else if (right = true) {
+            System.out.println("bike turns right");
+        } else {
+            System.out.println("bike goes straight forward"); 
+        }
+        */
+    }
+
+    static String Shape(String handleShape) {
+        return handleShape;
+
+
+    }
+}
